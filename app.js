f520fab2fcbe737dbc0f9b196b0e24a641a9c870
Franko Stipanov <fstipanov@gmail.com>
2021-05-18 11:59:00 +0200

    edit app.js

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
 'use strict'
+const selectBill = document.querySelector('#bill');
+const selectPercentage = document.querySelector('#percentage');
+const outputTip = document.querySelector('#tip');
+
+
+const bill = selectBill.value;
+const percentage = selectPercentage.value;
+// console.log(outputTip);
+
+
+console.log(bill);
+console.log(percentage);
+
+const btn = document.querySelector('.btn');
+const calcTip = (bill, percentage) => bill * percentage / 100;
+tip = calcTip(bill, percentage);
+btn.addEventListener('click', () => {
+
+})
