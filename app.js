cb23f546cd49a363c083052bee18545fcb4c1ab4
Franko Stipanov <fstipanov@gmail.com>
2021-05-18 12:42:02 +0200

    app.js working

--- app.js
+++ app.js
@@ -2,9 +2,11 @@
 const selectBill = document.querySelector('#bill');
 const selectPercentage = document.querySelector('#percentage');
 const outputTip = document.querySelector('#tip');
+const total = document.getElementById('total');
 
 
-const bill = selectBill.value;
+let bill = selectBill.value;
+bill = Number(bill);
 const percentage = selectPercentage.value;
 // console.log(outputTip);
 
@@ -16,5 +18,6 @@ const btn = document.querySelector('.btn');
 const calcTip = (bill, percentage) => bill * percentage / 100;
 tip = calcTip(bill, percentage);
 btn.addEventListener('click', () => {
-
+    outputTip.innerHTML = tip;
+    total.innerHTML = `${tip + bill}`;
 })
